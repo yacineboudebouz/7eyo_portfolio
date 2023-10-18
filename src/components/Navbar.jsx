f567f5546e7ee9e4f8da55ff342424b545adaa1f
Yacine Boudebouz <yacineboudebbouz7@gmail.com>
2023-10-18 13:58:48 +0200

    navbar finish 1.0

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
   const [toggle, setToggle] = useState(false)
   return (
     <nav className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 right-0 bg-primary`}>
-      <div className='w-full flex justify-between items-center max-w-7xl mx-auto'>
+      <div className='w-full flex justify-evenly items-center max-w-7xl mx-auto'>
         <Link to='/'
           className='flex items-center gap-2'
           onClick={() => {
@@ -18,7 +18,7 @@ const Navbar = () => {
           }}
         >
           <img src={logo} alt='logo' className='w-9 h-9 object-contain' />
-          <p className='text-white text-[18px] font-bold cursor-pointer'>Abdelhai <span className=' sm:block hidden'>| Mobile developer</span></p>
+          <p className='text-white text-[18px] font-bold cursor-pointer flex'>Abdelhai &nbsp;<span className=' sm:block hidden'>| Mobile developer</span></p>
         </Link>
         <ul className='list-none hidden sm:flex flex-row gap-10 '>
           {navLinks.map((link) => (
