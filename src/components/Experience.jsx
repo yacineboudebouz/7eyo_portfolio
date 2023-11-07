b2682ff60a9c6a05566f4bcaf4385e9e1d69545e
Yacine Boudebouz <yacineboudebbouz7@gmail.com>
2023-11-07 23:59:50 +0100

    ok

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -7,21 +7,28 @@ import { experiences } from '../constants'
 import { SectionWrapper } from '../hoc'
 import { textVariant } from '../utils/motion'
 
-const ExperienceCard = ({ experience }) => {
-  <VerticalTimelineElement
-    contentStyle={{ background: '#1d1836', color: '#fff' }}
-    contentArrowStyle={{ borderRight: '7px solid #232631' }}
-    date={experience.date}
-    iconStyle={{ background: experience.iconBg }}>
-    icon={
-      <div>
-        <img src={experience.icon}
-          alt={experience.company_name}
-          className='w-[60%] h-[60%]' />
-      </div>
-    }
-  </VerticalTimelineElement>
-}
+const ExperienceCard = ({ experience }) =>
+(<VerticalTimelineElement
+  contentStyle={{ background: '#1d1836', color: '#fff' }}
+  contentArrowStyle={{ borderRight: '7px solid #232631' }}
+  date={experience.date}
+  iconStyle={{ background: experience.iconBg }}
+  icon={
+    <div>
+      <img src={experience.icon}
+        alt={experience.company_name}
+        className='w-[60%] h-[60%]' />
+    </div>
+  }
+>
+  <div>
+    <h3 className=' text-white text-[24px]'>
+      {experience.title}
+    </h3>
+  </div>
+</VerticalTimelineElement>
+)
+
 
 const Experience = () => {
   return (
