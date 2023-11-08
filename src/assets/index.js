2b220be5f5205848d1bf58ab127ee17d0c290ebd
Yacine Boudebouz <yacineboudebbouz7@gmail.com>
2023-11-08 14:28:47 +0100

    experience

--- src/assets/index.js
+++ src/assets/index.js
@@ -21,11 +21,6 @@ import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
 import threejs from "./tech/threejs.svg";
 
-import meta from "./company/meta.png";
-import shopify from "./company/shopify.png";
-import starbucks from "./company/starbucks.png";
-import tesla from "./company/tesla.png";
-
 import carrent from "./carrent.png";
 import jobit from "./jobit.png";
 import tripguide from "./tripguide.png";
@@ -33,8 +28,12 @@ import tripguide from "./tripguide.png";
 import laravel from "./tech/laravel.png";
 import flutter from "./tech/flutter.png";
 import kotlin from "./tech/kotlin.png";
+import estin from "./estin.png";
+import upwork from "./upwork.png";
 
 export {
+  upwork,
+  estin,
   laravel,
   flutter,
   kotlin,
@@ -59,10 +58,6 @@ export {
   tailwind,
   typescript,
   threejs,
-  meta,
-  shopify,
-  starbucks,
-  tesla,
   carrent,
   jobit,
   tripguide,
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -14,10 +14,10 @@ const ExperienceCard = ({ experience }) =>
   date={experience.date}
   iconStyle={{ background: experience.iconBg }}
   icon={
-    <div>
+    <div className=' flex justify-center items-center w-full h-full'>
       <img src={experience.icon}
         alt={experience.company_name}
-        className='w-[60%] h-[60%]' />
+        className=' w-[60%] h-[60%]' />
     </div>
   }
 >
@@ -25,7 +25,16 @@ const ExperienceCard = ({ experience }) =>
     <h3 className=' text-white text-[24px]'>
       {experience.title}
     </h3>
+    <p className=' text-secondary text-[16px] font-semibold'>{experience.company_name}</p>
   </div>
+  <ul
+    className=' mt-5 list-disc ml-5 space-y-2'>
+    {experience.points.map((point, index) => (
+      <li key={`experience-point-${index}`} className='text-white text-[14px] pl-1 tracking-wider'>
+        {point}
+      </li>
+    ))}
+  </ul>
 </VerticalTimelineElement>
 )
 
@@ -35,7 +44,7 @@ const Experience = () => {
     <>
       <motion.div variants={textVariant()}>
         <p className={`${styles.heroSubText}  `} >What i have done </p>
-        <h2 className={styles.sectionHeadText}>Work Experience.</h2>
+        <h2 className={styles.sectionHeadText}>Experience.</h2>
       </motion.div>
       <div className='mt-10 flex flex-col'>
         <VerticalTimeline>
