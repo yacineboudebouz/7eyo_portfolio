33b28a5bd0e782dd888291a3abbaa6af5bd792f6
Yacine Boudebouz <yacineboudebbouz7@gmail.com>
2023-11-20 00:20:24 +0100

    Remove logo from Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -17,7 +17,7 @@ const Navbar = () => {
             window.scrollTo(0, 0);
           }}
         >
-          <img src={logo} alt='logo' className='w-9 h-9 object-contain' />
+
           <p className='text-white text-[18px] font-bold cursor-pointer flex'>Abdelhai &nbsp;<span className=' sm:block hidden'>| Mobile developer</span></p>
         </Link>
         <ul className='list-none hidden sm:flex flex-row gap-10 '>
